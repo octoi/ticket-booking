e0a75e72f26adfdf14da8e3d47e6cb9b46a29a43
Fadhil Saheer <fadhilsaheer@gmail.com>
2024-07-02 11:00:02 +0530

    Event repository methods

---
+++ api/repositories/event.go
@@ -0,0 +1,41 @@
+package repositories
+
+import (
+	"context"
+	"time"
+
+	"github.com/octoi/ticket-booking/models"
+)
+
+type EventRepository struct {
+	db any
+}
+
+func (r *EventRepository) GetMany(ctx context.Context) ([]*models.Event, error) {
+	events := []*models.Event{}
+
+	events = append(events, &models.Event{
+		ID:        "123456",
+		Name:      "My Fav Band",
+		Location:  "Somewhere, There",
+		Date:      time.Now(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+
+	return events, nil
+}
+
+func (r *EventRepository) GetOne(ctx context.Context, eventId string) (*models.Event, error) {
+	return nil, nil
+}
+
+func (r *EventRepository) CreateOne(ctx context.Context, event models.Event) (*models.Event, error) {
+	return nil, nil
+}
+
+func NewEventRepository(db any) models.EventRepository {
+	return &EventRepository{
+		db: db,
+	}
+}
