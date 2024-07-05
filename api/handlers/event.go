e139a3187f2ddbb36520bb6edfc0779acf96f1f6
Fadhil <fadhilsaheer@gmail.com>
2024-07-05 22:29:54 +0530

    update one and delete one handler

--- api/handlers/event.go
+++ api/handlers/event.go
@@ -85,6 +85,55 @@ func (h *EventHandler) CreateOne(ctx *fiber.Ctx) error {
 	})
 }
 
+func (h *EventHandler) UpdateOne(ctx *fiber.Ctx) error {
+	eventId, _ := strconv.Atoi(ctx.Params("eventId"))
+	updatedData := make(map[string]interface{})
+
+	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
+	defer cancel()
+
+	if err := ctx.BodyParser(&updatedData); err != nil {
+		return ctx.Status(fiber.StatusUnprocessableEntity).JSON(&fiber.Map{
+			"status":  "fail",
+			"message": err.Error(),
+			"data":    nil,
+		})
+	}
+
+	event, err := h.repository.UpdateOne(context, uint(eventId), updatedData)
+
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
+			"status":  "fail",
+			"message": err.Error(),
+		})
+	}
+
+	return ctx.Status(fiber.StatusCreated).JSON(&fiber.Map{
+		"status":  "success",
+		"message": "Event created",
+		"data":    event,
+	})
+}
+
+func (h *EventHandler) DeleteOne(ctx *fiber.Ctx) error {
+	eventId, _ := strconv.Atoi(ctx.Params("eventId"))
+
+	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
+	defer cancel()
+
+	err := h.repository.DeleteOne(context, uint(eventId))
+
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
+			"status":  "fail",
+			"message": err.Error(),
+		})
+	}
+
+	return ctx.SendStatus(fiber.StatusNoContent)
+}
+
 func NewEventHandler(router fiber.Router, repository models.EventRepository) {
 	handler := &EventHandler{
 		repository: repository,
@@ -93,4 +142,6 @@ func NewEventHandler(router fiber.Router, repository models.EventRepository) {
 	router.Get("/", handler.GetMany)
 	router.Post("/", handler.CreateOne)
 	router.Get("/:eventId", handler.GetOne)
+	router.Put("/:eventId", handler.UpdateOne)
+	router.Delete("/:eventId", handler.DeleteOne)
 }
