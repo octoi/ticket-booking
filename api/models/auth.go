c7111fd747caeca94d7ddadda95ace7e486801cc
Fadhil <fadhilsaheer@gmail.com>
2024-07-07 23:34:58 +0530

    auth model

---
+++ api/models/auth.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"context"
+	"golang.org/x/crypto/bcrypt"
+	"net/mail"
+)
+
+type AuthCredentials struct {
+	Email    string `json:"email" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+type User struct {
+}
+
+type AuthRepository interface {
+	RegisterUser(ctx context.Context, registerData *AuthCredentials) (*User, error)
+	GetUser(ctx context.Context, query interface{}, args ...interface{}) (*User, error)
+}
+
+type AuthService interface {
+	Login(ctx context.Context, loginData *AuthCredentials) (string, *User, error)
+	Register(ctx context.Context, registerData *AuthCredentials) (string, *User, error)
+}
+
+// Check if password matches hash
+
+func MatchesHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err != nil
+}
+
+// Checks if email is valid
+
+func IsValidEmail(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err != nil
+}
